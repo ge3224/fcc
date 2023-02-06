482153dde4b98083962e035d31aeb012557a34cc
ge3224 <ge3224@gmail.com>
2023-02-06 16:48:14 -0600

    Try second challenge

--- jads/sandbox/alg_1.js
+++ jads/sandbox/alg_1.js
@@ -1,13 +1,27 @@
 function celsiusToFahrenheit() {
-  let c = 30;
-  let f = c * 9.0 / 5 + 32;
+  const c = 30;
+  const f = c * 9.0 / 5 + 32;
   console.log(c, f);
+}
+
+function reverseAString() {
+  const str = "hello";
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  console.log(str, result);
 }
 
 function init() {
   const fifth = document.getElementById("005");
   if (fifth !== null && fifth !== undefined && fifth !== void 0) {
     fifth.addEventListener("click", celsiusToFahrenheit);
+  }
+
+  const sixth = document.getElementById("006");
+  if (sixth !== null && sixth !== undefined && sixth !== void 0) {
+    sixth.addEventListener("click", reverseAString);
   }
 }
 
