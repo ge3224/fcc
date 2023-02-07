5fed7d8c808699aa4402fa01b957b0f154a4ee18
ge3224 <ge3224@gmail.com>
2023-02-07 16:28:48 -0600

    Complete challenge 16

--- jads/sandbox/fn_prog.js
+++ jads/sandbox/fn_prog.js
@@ -56,10 +56,44 @@ function tabsChallengeHandler() {
   */
 }
 
+/**
+  * Complete the code for the squareList function using any combination of map(),
+  * filter(), and reduce(). The function should return a new array containing the
+  * squares of only the positive integers (decimal numbers are not integers) when
+  * an array of real numbers is passed to it. An example of an array of real numbers
+  * is [-3, 4.8, 5, 3, -3.2].
+  */
+const squareList = (arr) => {
+  const newArr = arr.filter(item => item > 0)
+    .filter(item => item % 1 === 0)
+    .map(item => Math.pow(item, 2));
+  return newArr;
+};
+
+function challenge16() {
+  const tests = [
+    [[4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2], [16, 1764, 36]],
+    [[-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3], [9, 100, 49]],
+  ];
+
+  tests.forEach(args => {
+    const result = squareList(args[0]);
+    let passing = "PASS";
+    for (let i = 0; i < result.length; i++) {
+      if (result[i] !== args[1][i]) {
+        passing = "FAIL";
+        break;
+      }
+    }
+    console.log(result, passing);
+  });
+}
+
 // initialize
 (() => {
   const ids = [
     ["022", tabsChallengeHandler],
+    ["023", challenge16],
   ];
 
   ids.map(item => {
