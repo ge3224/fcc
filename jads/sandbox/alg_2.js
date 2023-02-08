20a8cad238f0eb3ef618e718e8414dba824c643e
ge3224 <ge3224@gmail.com>
2023-02-08 15:20:19 -0600

    Handle fourth challenge, Intermediate Algorithm Scripting

--- jads/sandbox/alg_2.js
+++ jads/sandbox/alg_2.js
@@ -1,23 +1,53 @@
-// You will be provided with an initial array (the first argument in the 
-// destroyer function), followed by one or more arguments. Remove all elements 
-// from the initial array that are of the same value as these arguments.
-function destroyer(arr, ...args) {
-  return arr.filter(item => args.indexOf(item) === -1);
+function areMatchingObjects(obj1, obj2) {
+  let match = true;
+  for (let prop in obj2) {
+    if (obj1[prop] !== obj2[prop]) {
+      match = false;
+      break;
+    }
+  }
+  return match;
 }
 
-function ch3Handler() {
+function areMatchingArrays(arr1, arr2) {
+  let match = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      match = false;
+      break;
+    }
+  }
+  return match
+}
+
+function printResults(result, passing, assertion, label = "Output") {
+  const testMessage = (passing) ? "PASS" : `FAIL, should be: "${assertion}"`;
+  console.log(`${label}: ${result}\n::${testMessage}`);
+}
+
+function sumAll(arr) {
+  const newArr = arr.slice().sort((a, b) => a - b);
+  let sum = 0;
+
+  for (let i = newArr[0]; i <= newArr[newArr.length - 1]; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+function ch1Handler() {
   const tests = [
-    [[[1, 2, 3, 1, 2, 3], 2, 3], [1, 1]],
-    [[[1, 2, 3, 5, 1, 2, 3], 2, 3], [1, 5, 1]],
-    [[[3, 5, 1, 2, 2], 2, 3, 5], [1]],
-    [[[2, 3, 2, 3], 2, 3], []],
-    [[["tree", "hamburger", 53], "tree", 53], ["hamburger"]],
-    [[["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan"], [12, 92, 65]],
+    [[1, 4], 10],
+    [[4, 1], 10],
+    [[5, 10], 45],
+    [[10, 5], 45],
   ];
 
   tests.forEach(args => {
-    const result = destroyer(args[0][0], ...args[0].slice(1));
-    printResults(result, areMatch(result, args[1]), args[1], "destroyer");
+    const result = sumAll(args[0]);
+    const passing = (result === args[1]) ? true : false;
+    printResults(result, passing, args[1], "sumAll");
   });
 }
 
@@ -49,51 +79,86 @@ function ch2Handler() {
 
   tests.forEach(args => {
     const result = diffArray(args[0], args[1]);
-    const passing = areMatch(result, args[2]);
+    const passing = areMatchingArrays(result, args[2]);
     printResults(result, passing, args[2], "diffArray");
   });
 }
 
-function sumAll(arr) {
-  const newArr = arr.slice().sort((a, b) => a - b);
-  let sum = 0;
-
-  for (let i = newArr[0]; i <= newArr[newArr.length - 1]; i++) {
-    sum += i;
-  }
-
-  return sum;
+// You will be provided with an initial array (the first argument in the 
+// destroyer function), followed by one or more arguments. Remove all elements 
+// from the initial array that are of the same value as these arguments.
+function destroyer(arr, ...args) {
+  return arr.filter(item => args.indexOf(item) === -1);
 }
 
-function ch1Handler() {
+function ch3Handler() {
   const tests = [
-    [[1, 4], 10],
-    [[4, 1], 10],
-    [[5, 10], 45],
-    [[10, 5], 45],
+    [[[1, 2, 3, 1, 2, 3], 2, 3], [1, 1]],
+    [[[1, 2, 3, 5, 1, 2, 3], 2, 3], [1, 5, 1]],
+    [[[3, 5, 1, 2, 2], 2, 3, 5], [1]],
+    [[[2, 3, 2, 3], 2, 3], []],
+    [[["tree", "hamburger", 53], "tree", 53], ["hamburger"]],
+    [[["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan"], [12, 92, 65]],
   ];
 
   tests.forEach(args => {
-    const result = sumAll(args[0]);
-    const passing = (result === args[1]) ? true : false;
-    printResults(result, passing, args[1], "sumAll");
+    const result = destroyer(args[0][0], ...args[0].slice(1));
+    printResults(result, areMatchingArrays(result, args[1]), args[1], "destroyer");
   });
 }
 
-function areMatch(arr1, arr2) {
-  let match = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      match = false;
-      break;
+// Make a function that looks through an array of objects (first argument) and 
+// returns an array of all objects that have matching name and value pairs 
+// (second argument). Each name and value pair of the source object has to be 
+// present in the object from the collection if it is to be included in the 
+// returned array.
+function whatIsInAName(collection, source) {
+  const newArr = collection.filter(obj => {
+    let match = true;
+
+    for (let prop in source) {
+      if (obj[prop] === undefined || obj[prop] !== source[prop]) {
+        match = false;
+        break;
+      }
     }
-  }
-  return match
+
+    if (match) {
+      return obj;
+    }
+  });
+
+  return newArr;
 }
 
-function printResults(result, passing, assertion, label = "Output") {
-  const testMessage = (passing) ? "PASS" : `FAIL, should be: "${assertion}"`;
-  console.log(`${label}: ${result}\n::${testMessage}`);
+function ch4Handler() {
+  const tests = [
+    [[[{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }], [{ first: "Tybalt", last: "Capulet" }]],
+    [[[{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }], [{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }]],
+    [[[{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }], [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }]],
+    [[[{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }], [{ "apple": 1, "bat": 2, "cookie": 2 }]],
+    [[[{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat": 2 }], { "apple": 1, "bat": 2 }], [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }]],
+    [[[{ "a": 1, "b": 2, "c": 3 }], { "a": 1, "b": 9999, "c": 3 }], []],
+    [[[{ "a": 1, "b": 2, "c": 3, "d": 9999 }], { "a": 1, "b": 9999, "c": 3 }], []],
+  ];
+
+  tests.forEach(args => {
+    const result = whatIsInAName(args[0][0], args[0][1]);
+    const passing = true;
+    for (let i = 0; i < result.length; i++) {
+      if (!areMatchingObjects(result[i], args[1][0])) {
+        passing = false;
+        break;
+      }
+    }
+
+    printResults(
+      result,
+      passing,
+      args[1][0],
+      "whatIsInAName",
+    );
+  });
 }
 
 (() => {
@@ -101,6 +166,7 @@ function printResults(result, passing, assertion, label = "Output") {
     ["026", ch1Handler],
     ["027", ch2Handler],
     ["028", ch3Handler],
+    ["029", ch4Handler],
   ];
 
   ids.map(item => {
