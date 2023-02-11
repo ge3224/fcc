be5f8cd62a9108c7a8a457127e720df4a3318264
ge3224 <ge3224@gmail.com>
2023-02-11 15:01:30 -0600

    Complete final IAT challenge

--- jads/sandbox/src/alg_2.js
+++ jads/sandbox/src/alg_2.js
@@ -757,6 +757,38 @@ function ch20Handler() {
   });
 }
 
+// Return a new array that transforms the elements' average altitude into their 
+// orbital periods (in seconds).
+//
+// The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+//
+// The values should be rounded to the nearest whole number. The body being 
+// orbited is Earth.
+//
+// The radius of the earth is 6367.4447 kilometers, and the GM value of earth 
+// is 398600.4418 km3s-2.
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+
+  return arr.map((satellite) => {
+    const a = earthRadius + satellite.avgAlt;
+    return {
+      name: satellite.name,
+      orbitalPeriod: Math.round(2 * Math.PI * Math.sqrt(Math.pow(a, 3) / GM)),
+    }
+  });
+}
+
 function ch21Handler() {
-  console.log("TODO");
+  const tests = [
+    assertion(orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]), [{ name: "sputnik", orbitalPeriod: 86400 }]),
+    assertion(orbitalPeriod([{ name: "iss", avgAlt: 413.6 }, { name: "hubble", avgAlt: 556.7 }, { name: "moon", avgAlt: 378632.553 }]), [{ name: "iss", orbitalPeriod: 5557 }, { name: "hubble", orbitalPeriod: 5734 }, { name: "moon", orbitalPeriod: 2377399 }]),
+  ];
+
+  checkResults(tests, (left, right) => {
+    const a = JSON.stringify(left);
+    const b = JSON.stringify(right);
+    console.log((a === b) ? "PASS" : `FAIL:\n\texpected${b}\n\tgot: ${a}`);
+  });
 }
