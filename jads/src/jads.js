2a93a264d0adcb0e28e033b556c20b3139ae4a37
ge3224 <ge3224@gmail.com>
2023-02-12 07:44:18 -0600

    Finish cp4

--- jads/src/jads.js
+++ jads/src/jads.js
@@ -185,8 +185,66 @@ function cp3Handler() {
   });
 }
 
+
+// Return true if the passed string looks like a valid US phone number.
+//
+// The user may fill out the form field any way they choose as long as it has 
+// the format of a valid US number. The following are examples of valid formats 
+// for US numbers (refer to the tests below for other variants):
+//
+// 555-555-5555
+// (555)555-5555
+// (555) 555-5555
+// 555 555 5555
+// 5555555555
+// 1 555 555 5555
+//
+// For this challenge you will be presented with a string such as 800-692-7753 
+// or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone 
+// number based on any combination of the formats provided above. The area code 
+// is required. If the country code is provided, you must confirm that the 
+// country code is 1. Return true if the string is a valid US phone number; 
+// otherwise return false.
+function telephoneCheck(str) {
+  let reg = /^(1)?(\s)?(\d{3}|\(\d{3}\))(\s|-|\.)?(\d{3})(\s|-|\.)?(\d{4})$/;
+  return reg.test(str);
+}
+
 function cp4Handler() {
-  console.log("Hello, from cp4!");
+  const tests = [
+    assertion(telephoneCheck("1 555-555-5555"), true),
+    assertion(telephoneCheck("1 (555) 555-5555"), true),
+    assertion(telephoneCheck("5555555555"), true),
+    assertion(telephoneCheck("555-555-5555"), true),
+    assertion(telephoneCheck("(555)555-5555"), true),
+    assertion(telephoneCheck("1(555)555-5555"), true),
+    assertion(telephoneCheck("555-5555"), false),
+    assertion(telephoneCheck("5555555"), false),
+    assertion(telephoneCheck("1 555)555-5555"), false),
+    assertion(telephoneCheck("1 555 555 5555"), true),
+    assertion(telephoneCheck("1 456 789 4444"), true),
+    assertion(telephoneCheck("123**&!!asdf#"), false),
+    assertion(telephoneCheck("55555555"), false),
+    assertion(telephoneCheck("(6054756961)"), false),
+    assertion(telephoneCheck("2 (757) 622-7382"), false),
+    assertion(telephoneCheck("0 (757) 622-7382"), false),
+    assertion(telephoneCheck("-1 (757) 622-7382"), false),
+    assertion(telephoneCheck("2 757 622-7382"), false),
+    assertion(telephoneCheck("10 (757) 622-7382"), false),
+    assertion(telephoneCheck("27576227382"), false),
+    assertion(telephoneCheck("(275)76227382"), false),
+    assertion(telephoneCheck("2(757)6227382"), false),
+    assertion(telephoneCheck("2(757)622-7382"), false),
+    assertion(telephoneCheck("555)-555-5555"), false),
+    assertion(telephoneCheck("(555-555-5555"), false),
+    assertion(telephoneCheck("(555)5(55?)-5555"), false),
+    assertion(telephoneCheck("55 55-55-555-5"), false),
+    assertion(telephoneCheck("11 555-555-5555"), false),
+  ];
+
+  checkResults(tests, (left, right) => {
+    console.log((left === right) ? "PASS" : `FAIL:\n\texpected: ${right}\n\tgot: ${left}`);
+  });
 }
 
 function cp5Handler() {
