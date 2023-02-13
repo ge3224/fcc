c322a0a91bb0b59ca8cdfaeeadd73d055eaa34de
ge3224 <ge3224@gmail.com>
2023-02-13 10:28:11 -0600

    Complete final project

--- jads/src/jads.js
+++ jads/src/jads.js
@@ -279,35 +279,51 @@ function cp4Handler() {
 // | One-hundred Dollars	| $100 (ONE HUNDRED) |
 //
 function checkCashRegister(price, cash, cid) {
-  const message = {
+
+  const change = [];
+
+  const coinsAndBills = {
+    "ONE HUNDRED": 100,
+    "TWENTY": 20,
+    "TEN": 10,
+    "FIVE": 5,
+    "ONE": 1,
+    "QUARTER": 0.25,
+    "DIME": 0.1,
+    "NICKEL": 0.05,
+    "PENNY": 0.01,
+  }
+
+  const messages = {
     open: "OPEN",
     closed: "CLOSED",
     insufficient: "INSUFFICIENT_FUNDS",
   }
 
-  const change = [];
+  // determine how much change is owed.
+  let owe = +((cash - price).toFixed(2));
 
-  // return early if not enough cash was provided, or no change is owe.
-  if (price >= cash) {
+  // check cash register for sufficient funds
+  let cashInDrawer = cid.reduce((subtotal, slot) => {
+    const divisible = owe / coinsAndBills[slot[0]];
+    if (divisible > 1) {
+      subtotal += slot[1];
+    }
+    return +(subtotal.toFixed(2));
+  }, 0);
+
+  if (cashInDrawer < owe) {
     return {
-      status: message.closed,
+      status: messages.insufficient,
       change: change,
     }
   }
 
-  let owe = +((cash - price).toFixed(2));
-
-  // check cash register for sufficient funds
-  let onhand = cid.reduce((subtotal, slot) => {
-    subtotal += slot[1];
-    return +(subtotal.toFixed(2));
-  }, 0);
-
-  if (onhand < owe) {
+  if (cashInDrawer === owe) {
     return {
-      status: message.insufficient,
-      change: change,
-    }
+      status: messages.closed,
+      change: cid,
+    };
   }
 
   // calculate change
@@ -317,21 +333,8 @@ function checkCashRegister(price, cash, cid) {
     drawer[prop] = slot[1];
   });
 
-  const types = [
-    ["ONE HUNDRED", 100],
-    ["TWENTY", 20],
-    ["TEN", 10],
-    ["FIVE", 5],
-    ["ONE", 1],
-    ["QUARTER", 0.25],
-    ["DIME", 0.1],
-    ["NICKEL", 0.05],
-    ["PENNY", 0.01]
-  ];
-
-  for (let i = 0; i < types.length; i++) {
-    const type = types[i][0];
-    const multiple = types[i][1];
+  for (let type in coinsAndBills) {
+    const multiple = coinsAndBills[type];
 
     if (owe >= multiple) {
       const subtotal = [type];
@@ -356,7 +359,7 @@ function checkCashRegister(price, cash, cid) {
   }
 
   return {
-    status: message.open,
+    status: messages.open,
     change: change,
   }
 }
@@ -367,7 +370,7 @@ function cp5Handler() {
     assertion(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]), { status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]] }),
     assertion(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]), { status: "INSUFFICIENT_FUNDS", change: [] }),
     assertion(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]), { status: "INSUFFICIENT_FUNDS", change: [] }),
-    // assertion(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]), { status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]] }),
+    assertion(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]), { status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]] }),
   ];
 
   checkResults(tests, (left, right) => {
